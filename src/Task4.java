336f6e0d0fd77f7b56c5fca0648e29421804057d
Saveli <tuponogov.90@mail.ru>
2025-04-27 20:40:55 +0300

    4 задача правки

--- src/Task4.java
+++ src/Task4.java
@@ -14,6 +14,7 @@ public class Task4 {
 
     public void start() {
         try {
+            boolean isEqual = true;
             List<String> allLines = Files.readAllLines(pathFile);
 
             if(allLines.size() != 6){
@@ -22,6 +23,7 @@ public class Task4 {
             }
 
             Map<String,Integer> sides = new HashMap<>();
+            Map<Integer,Integer> valueCount = new HashMap<>();
             for (String line : allLines) {
                 int a = Integer.parseInt(line.split(" ")[0]);
                 int b = Integer.parseInt(line.split(" ")[1]);
@@ -29,15 +31,26 @@ public class Task4 {
                 int max = Math.max(a, b);
                 int min = Math.min(a, b);
 
+                if(max < 1 || max > 1000 || min < 1 || min > 1000){
+                    System.out.println("Невозможно!");
+                    return;
+                }
+
+                if(max != min){
+                    isEqual = false;
+                }
+
                 String key = max + "x" + min;
+                valueCount.put(max, valueCount.getOrDefault(max, 0) + 1);
+                valueCount.put(min, valueCount.getOrDefault(min, 0) + 1);
                 sides.put(key, sides.getOrDefault(key,0) + 1);
             }
             
-            if(sides.values().stream().anyMatch(value -> value % 2 != 0)){
+            if(sides.values().stream().anyMatch(value -> value % 2 != 0) || (!isEqual && sides.size() == 1)){
                 System.out.println("Невозможно!");
                 return;
             }
-            
+
             String pastKey = "";
             for (String key : sides.keySet()) {
                 boolean isComplianceSide = false;
@@ -52,6 +65,13 @@ public class Task4 {
                 }
 
                 if(isComplianceSide == false){
+                    System.out.println("Невозможно!");
+                    return;
+                }
+            }
+
+            for (Integer value : valueCount.values()) {
+                if(value % 4 != 0){
                     System.out.println("Невозможно!");
                     return;
                 }
